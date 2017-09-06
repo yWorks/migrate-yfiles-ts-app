64f2baeaae887fe0eab836b2bb0b3792558f66dc
Lukas Holländer <lukas.hollaender@yworks.com>
2017-09-06 15:13:11 +0200

    changePropertyTypeRule

---
+++ customRules/changePropertyTypeRule.ts
@@ -0,0 +1,37 @@
+import * as ts from "typescript";
+import * as Lint from "tslint";
+import {changes} from "../changes"
+import {MemberRuleWalker} from "./memberRuleWalker";
+import {getFullyQualifiedName} from "./util";
+
+export class Rule extends Lint.Rules.TypedRule {
+  public applyWithProgram(sourceFile: ts.SourceFile, program: ts.Program): Lint.RuleFailure[] {
+    return this.applyWithWalker(new ChangeReturnTypeWalker(sourceFile, this.getOptions(), program));
+  }
+}
+
+class ChangeReturnTypeWalker extends MemberRuleWalker {
+  protected checkForChanges(node: ts.Node, oldParentName: string, oldMemberName: string) {
+    if (node.kind !== ts.SyntaxKind.SetAccessor
+        && node.kind !== ts.SyntaxKind.GetAccessor
+        && node.kind !== ts.SyntaxKind.PropertyAccessExpression
+        && node.kind !== ts.SyntaxKind.ElementAccessExpression) {
+      return;
+    }
+
+    const newType = changes["propertyTypeChanges"][oldParentName] && changes["propertyTypeChanges"][oldParentName][oldMemberName];
+    if (newType) {
+      const checker = this.getTypeChecker();
+
+      let oldType: string;
+      if (node.kind === ts.SyntaxKind.GetAccessor) {
+        oldType = getFullyQualifiedName(checker.getSignatureFromDeclaration(<ts.SignatureDeclaration>node).getReturnType(), checker)
+      } else {
+        oldType = getFullyQualifiedName(checker.getTypeAtLocation(node), checker);
+      }
+
+      this.addFailureAtNode(node,
+          `The type of property "${oldParentName}#${oldMemberName}" has changed from "${oldType}" to "${newType}"`);
+    }
+  }
+}
