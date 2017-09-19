1b9a3bed685a49292c96f003f26d6e4ce8009eeb
Lukas Holländer <lukas.hollaender@yworks.com>
2017-09-19 15:21:09 +0200

    also consider alias imports for namespace rules

--- customRules/namespaceRuleWalker.ts
+++ customRules/namespaceRuleWalker.ts
@@ -2,6 +2,11 @@ import * as ts from "typescript";
 import * as Lint from "tslint";
 
 export abstract class NamespaceRuleWalker extends Lint.ProgramAwareRuleWalker {
+  protected visitImportEqualsDeclaration(node: ts.ImportEqualsDeclaration): void {
+    this.checkNamespace(node, node.moduleReference.getText());
+    super.visitImportEqualsDeclaration(node);
+  }
+
   protected visitPropertyAccessExpression(node: ts.PropertyAccessExpression): void {
     this.checkNamespace(node, node.getText());
     super.visitPropertyAccessExpression(node);
